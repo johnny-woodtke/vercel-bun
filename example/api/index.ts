705bd11c8bfdf92f913f6ceaf3972a6cd3625df8
johnny-woodtke <johnnywoodtke@gmail.com>
2025-05-22 20:09:00 -0400

    try this

--- example/api/index.ts
+++ example/api/index.ts
@@ -1,6 +1,6 @@
 import { Elysia, t } from "elysia";
 
-const app = new Elysia({ prefix: "/api/index" }).get(
+const app = new Elysia({ prefix: "/api" }).get(
   "/",
   () => `Hello from bun@${Bun.version}`,
   {
