20c827d5c48416c7526cf94aded3c441ffaa78d2
johnny-woodtke <johnnywoodtke@gmail.com>
2025-05-22 20:27:49 -0400

    add another route

--- example/api/index.ts
+++ example/api/index.ts
@@ -1,12 +1,18 @@
 import { Elysia, t } from "elysia";
 
-const app = new Elysia({ prefix: "/api" }).get(
-  "/",
-  () => `Hello from bun@${Bun.version}`,
-  {
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => `Hello from bun@${Bun.version}`, {
     response: t.String(),
-  }
-);
+  })
+  .get(
+    "/hello",
+    ({ params }) => ({
+      message: `Hello ${params.firstName} ${params.lastName}`,
+    }),
+    {
+      params: t.Object({ firstName: t.String(), lastName: t.String() }),
+    }
+  );
 
 const isDev = process.env.NODE_ENV !== "production";
 
