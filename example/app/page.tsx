7fff1dcfe6336c91cb993630ae5a1283610e9350
johnny-woodtke <johnnywoodtke@gmail.com>
2025-05-30 17:39:31 -0400

    use next theme

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -6,7 +6,7 @@ import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4">
+    <div className="min-h-screen bg-background py-8 px-4">
       <div className="max-w-4xl mx-auto space-y-6">
         <Header />
         <SessionCard />
