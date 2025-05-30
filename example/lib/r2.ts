0de0d22a73661d0bc3bf8d4746c758fb5169d211
johnny-woodtke <johnnywoodtke@gmail.com>
2025-05-30 14:34:59 -0400

    log the vars

--- example/lib/r2.ts
+++ example/lib/r2.ts
@@ -1,5 +1,14 @@
 import { S3Client } from "bun";
 
+const config = {
+  accessKeyId: Bun.env.NEXT_PUBLIC_R2_ACCESS_KEY,
+  secretAccessKey: Bun.env.R2_SECRET_ACCESS_KEY,
+  bucket: Bun.env.NEXT_PUBLIC_R2_BUCKET,
+  endpoint: Bun.env.NEXT_PUBLIC_R2_ENDPOINT,
+};
+
+console.log("config", config);
+
 export const r2 = new S3Client({
   accessKeyId: Bun.env.NEXT_PUBLIC_R2_ACCESS_KEY,
   secretAccessKey: Bun.env.R2_SECRET_ACCESS_KEY,
