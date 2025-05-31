adfb99acbd27f2f3545b5389fa3ed15e5c91642a
johnny-woodtke <johnnywoodtke@gmail.com>
2025-05-30 20:31:15 -0400

    use susepense

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -3,6 +3,7 @@ import { SessionCard } from "@/components/session-card";
 import { AddEntryCard } from "@/components/add-entry-card";
 import { EntriesTable } from "@/components/entries-table";
 import { Footer } from "@/components/footer";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -10,9 +11,11 @@ export default function Home() {
       <Header />
       <main className="flex-1 py-8 px-4">
         <div className="max-w-4xl mx-auto space-y-6">
-          <SessionCard />
-          <AddEntryCard />
-          <EntriesTable />
+          <Suspense>
+            <SessionCard />
+            <AddEntryCard />
+            <EntriesTable />
+          </Suspense>
         </div>
       </main>
       <Footer />
