a9e94d7aae5d44b30e4dd754ad5686dd85b193ad
johnny-woodtke <johnnywoodtke@gmail.com>
2025-06-13 15:47:42 -0400

    move examples into single directory

---
+++ examples/vanilla/e2e/utils.ts
@@ -0,0 +1,9 @@
+export function getApiClient() {
+  const domain = Bun.env.E2E_VANILLA_API_DOMAIN;
+  if (!domain) {
+    throw new Error("E2E_VANILLA_API_DOMAIN environment variable is required");
+  }
+
+  return (path: `/api/${string}`, opts?: RequestInit) =>
+    fetch(`${domain}${path}`, opts);
+}
